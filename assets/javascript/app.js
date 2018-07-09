af879048e910ebcd2d6e068c1e165ebcf7055945
Amrit Singh <Amrit.Singh@hli.ubc.ca>
2018-07-09 15:13:28 -0400

    displayQuestion function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,20 +8,37 @@ $(document).ready(function() {
 
   // create 5 questions
   questions = [
-    Question('question 1', ['a', 'b', 'c'], (correct = 'a')),
-    Question('question 2', ['a', 'b', 'c'], 'a'),
-    Question('question 3', ['d', 'e', 'f', 'g'], 'g'),
-    Question('question 4', ['d', 'e', 'f'], 'e'),
-    Question('question 5', ['a', 'b', 'c', 'd', 'e'], 'a')
+    new Question('question 1', ['a', 'b', 'c'], 'a'),
+    new Question('question 2', ['a', 'b', 'c'], 'a'),
+    new Question('question 3', ['d', 'e', 'f', 'g'], 'g'),
+    new Question('question 4', ['d', 'e', 'f'], 'e'),
+    new Question('question 5', ['a', 'b', 'c', 'd', 'e'], 'a')
   ];
+
+  var questionCounter = 0;
 
   // step 1: upon clicking start button - hide start button and display first question and answer choices with Time remaining
   $('#start').on('click', function() {
-    console.log($(this));
-    $(this).hide()
-    displayFirstQuestion();
+    $(this).hide();
+    displayQuestion();
   });
 
+  function displayQuestion() {
+    html = '';
+    for (key in questions[questionCounter].answers) {
+      html += `<p class = 'answer'>${key}: ${
+        questions[questionCounter].answers[key]
+      }</p>`;
+    }
+    console.log(html);
+
+    $('#trivia').html(
+      `<p> Time Remaining: 30 seconds </p> <h2> ${
+        questions[questionCounter].question
+      }</h2> <p> ${html}`
+    );
+    console.log(questions[questionCounter].question);
+  }
 
   // step2: time counts down from 30 to 0
   // step3: if answer choice is clicked determine if answer is right or wrong - record wins/losses and display the correct answer to user for a short time before displaying the next question.
